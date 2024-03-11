64f07ab31e8d7e069c7e30d4de0f4988208afb97
Alin1233 <herciualin10@gmail.com>
2024-03-11 11:43:32 +0100

    Initial commit

---
+++ project-sortir-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import TestCompo from "./components/TestCompo"
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Hello world
+      </div>
+      <TestCompo/>
+    </>
+  )
+}
+
+export default App
---
+++ project-sortir-frontend/src/components/TestCompo.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const TestCompo = () => {
+
+    const test = 100
+  return (
+    <div>TestCompo
+        <p>{test}</p>
+    </div>
+  )
+}
+
+export default TestCompo
