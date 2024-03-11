7a3624cd66819ba5eff94a73bbe422cb99c50955
Alin1233 <herciualin10@gmail.com>
2024-03-11 17:15:29 +0100

    système de routage

--- project-sortir-frontend/src/App.jsx
+++ project-sortir-frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import TestCompo from "./components/TestCompo"
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import Accueil from './pages/Accueil'
+import Profile from './pages/Profile'
+import SeConnecter from './pages/SeConnecter'
 
 function App() {
   
 
-  return (
-    <>
-      <div>
-        Hello world
-      </div>
-      <TestCompo/>
-    </>
-  )
+    return (
+      <>
+      <Router>
+          <div>
+            <Link  to="/">Accueil</Link>
+            <Link  to="/profile">Profile</Link>
+            <Link  to="/connecter">Se Connecter</Link>
+          </div>
+          <Routes>
+            <Route path="/" element={<Accueil />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connecter" element={<SeConnecter />} />
+          </Routes>
+        </Router>
+      </>
+    )
 }
 
 export default App
