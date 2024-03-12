fb63f2fece9a8325094209ad7988ddd792baaf10
Alin1233 <herciualin10@gmail.com>
2024-03-12 08:43:22 +0100

    Update SeConnecter.jsx

--- project-sortir-frontend/src/pages/SeConnecter.jsx
+++ project-sortir-frontend/src/pages/SeConnecter.jsx
@@ -1,8 +1,25 @@
-
+import { useState } from "react";
 
 const SeConnecter = () => {
+
+  const[identifiant, setIdentifiant] = useState(' ');
+  const[motdepasse, setMotdepasse] = useState(' ');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(identifiant);
+    console.log(motdepasse);
+  };
+
   return (
-    <div>SeConnecter</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Identifant:</label>
+        <input type='text' name='identifiant' value={identifiant} onChange={(e) => setIdentifiant(e.target.value)}></input>
+        <label>Mot de passe:</label>
+        <input type='text' name='motdepasse' value={motdepasse} onChange={(e) => setMotdepasse(e.target.value)}></input>
+        <button type="submit">Connexion</button>
+      </form>
+    </div>
   )
 }
 
