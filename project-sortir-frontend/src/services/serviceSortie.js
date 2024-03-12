b966a14a2b4acca2f944da65eca4f8a018d3599f
Alin1233 <herciualin10@gmail.com>
2024-03-12 20:26:58 +0100

    Create serviceSortie.js

---
+++ project-sortir-frontend/src/services/serviceSortie.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:8000'
+const creerSortie = async (data) => {
+    const url = baseUrl+'/connecter'
+    try {
+        const response = await axios.post(url,data)
+        return response
+    } catch (error) {
+        console.error(error);
+    }
+}
+export default {creerSortie}
